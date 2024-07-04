412063556bf482c0527a609daf72aafad1148d82
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-03 21:19:48 -0400

    chore: check for extra tags in webhook handler

--- features/githubHandler.ts
+++ features/githubHandler.ts
@@ -28,7 +28,7 @@ export async function githubWebhookHandler(json: any) {
     // find user in db
     const user = await db.select().from(schema.users).where(like(schema.users.githubUser, (json.pusher.name as string).trim()))
 
-    if (user.length !== 0) {
+    if (user.length !== 0 && user[0].installed === 2 && user[0].threadTS && user[0].expireTime && - Date.now() > 0) {
         console.log("Sending commit to thread:", user[0].threadTS)
         // send the commits to the thread
         await slackClient.chat.postMessage({
@@ -46,7 +46,7 @@ export async function githubWebhookHandler(json: any) {
             ]
         })
     } else {
-        console.log("No user found for commit: ", json.pusher.name)
+        blog(`No user found for commit: ${json.pusher.name} on ${json.repository.full_name} or arcade session expired! time till finished: ${user[0].expireTime} - ${- Date.now()}`, "error")
     }
     return new Response("ok", { status: 200 });
 }
