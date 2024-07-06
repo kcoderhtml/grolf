1347e7d1fb1086d08a882986ca6a325b77889083
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-06 11:28:39 -0400

    bug: fix the url for releases only pointing to the head commit

--- features/githubHandler.ts
+++ features/githubHandler.ts
@@ -47,7 +47,7 @@ export async function githubWebhookHandler(json: any) {
                     type: "section",
                     text: {
                         type: "mrkdwn",
-                        text: `<${json.head_commit.url}|${json.pusher.name} ${(json.ref as string).startsWith("refs/tags/") ? `released new version:> \`${(json.ref as string).split("/")[2]}\`` : `committed:> \`${(json.head_commit.message as string).split("\n")[0].trim().replaceAll("`", "")}\``} on <${json.repository.html_url}|${json.repository.full_name}>`
+                        text: `<${(json.ref as string).startsWith("refs/tags/") ? json.repository.html_url : `https://github.com/${json.repository.full_name}/commit/${json.head_commit.id}`}|${json.pusher.name} ${(json.ref as string).startsWith("refs/tags/") ? `released new version:> \`${(json.ref as string).split("/")[2]}\`` : `committed:> \`${(json.head_commit.message as string).split("\n")[0].trim().replaceAll("`", "")}\``} on <${json.repository.html_url}|${json.repository.full_name}>`
                     }
                 }
             ]
