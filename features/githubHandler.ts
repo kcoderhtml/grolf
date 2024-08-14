28bb38338850abfa59476f102d1183f6efa0a0d0
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-14 11:27:43 -0400

    feat: ignore new repos being created

--- features/githubHandler.ts
+++ features/githubHandler.ts
@@ -43,7 +43,7 @@ export async function githubHandler(request: Request) {
 export async function githubWebhookHandler(json: any) {
   const isRelease = (json.ref as string).startsWith("refs/tags/");
   // check if the push is a commit or a release
-  if (json.pusher.name === "github-actions[bot]")
+  if (json.pusher.name === "github-actions[bot]" || json.action === "added")
     return new Response("ok", { status: 200 });
 
   if (isRelease) {
